af9563c4b3c948a4416c12e01d472117d0a93047
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-23 10:13:20 -0600

    updated space css

---
+++ space/space.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Space</title>
+		<link rel="stylesheet" href="css/styles.css" type="text/css">
+	</head>
+	<body>
+		<h1>Space</h1>
+		<p>
+			<span id="fog">A long</span>
+			<span id="fog-2">long time</span>
+			<span id="glow">ago,</span>
+			<span id="light">in a</span>
+			<span id="stars">galaxy</span>
+			<span id="stars-2">far</span>
+			<span id="small-stars">far</span>
+			<span id="small-stars-2">away&hellip;</span>
+		</p>
+	</body>
+</html>
