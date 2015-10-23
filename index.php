61fd089044a06c918f166bafdbfb41fbb88a9717
Jennifer Hung <jennifersukay505@gmail.com>
2015-10-23 09:44:30 -0600

    added space css

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-		<title>Index</title>
+		<meta charset="UTF-8">
+		<title>CSS Animations</title>
+		<link rel="stylesheet" href="css/space.css" type="text/css">
 	</head>
 	<body>
-		<p>Index Page</p>
+		<h1>Space</h1>
+		<p>
+			<span id="fog">A long</span>
+			<span id="fog-2">long time</span>
+			<span id="glow">ago,</span>
+			<span id="light">in a</span>
+			<span id="stars">galaxy</span>
+			<span id="stars-2">far</span>
+			<span id="small-stars">far</span>
+			<span id="small-stars-2">away&hellip;</span>
+		</p>
 	</body>
 </html>
